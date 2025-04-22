04a4b9e38a55691574540114d029a74d580ca7e9
Niveath <a.niveath@gmail.com>
2025-04-22 14:34:31 +0530

    Updated trace downloader and reuse cache for multicore

--- download_spec2k17.sh
+++ download_spec2k17.sh
@@ -1,79 +1,63 @@
 #!/bin/bash
 
-# Download the memory intensive SPEC CPU2017 traces
-# 
-# @Author: Navarro Torres, Agustín
-# @Date: 25/07/2022
-#
+# Download the SPEC CPU2017 traces in parallel
 
-DIR="$1"
-URL="https://dpc3.compas.cs.stonybrook.edu/champsim-traces/speccpu/"
+BASE_URL="https://dpc3.compas.cs.stonybrook.edu/champsim-traces/speccpu/"
 TRACES=(
-    '602.gcc_s-734B.champsimtrace.xz'
-    '602.gcc_s-1850B.champsimtrace.xz'
-    '602.gcc_s-2226B.champsimtrace.xz'
-    '603.bwaves_s-891B.champsimtrace.xz'
-    '603.bwaves_s-1740B.champsimtrace.xz'
-    '603.bwaves_s-2609B.champsimtrace.xz'
-    '603.bwaves_s-2931B.champsimtrace.xz'
-    '605.mcf_s-472B.champsimtrace.xz'
-    '605.mcf_s-484B.champsimtrace.xz'
-    '605.mcf_s-665B.champsimtrace.xz'
-    '605.mcf_s-782B.champsimtrace.xz'
-    '605.mcf_s-994B.champsimtrace.xz'
-    '605.mcf_s-1152B.champsimtrace.xz'
-    '605.mcf_s-1536B.champsimtrace.xz'
-    '605.mcf_s-1554B.champsimtrace.xz'
-    '605.mcf_s-1644B.champsimtrace.xz'
-    '607.cactuBSSN_s-2421B.champsimtrace.xz'
-    '607.cactuBSSN_s-3477B.champsimtrace.xz'
-    '607.cactuBSSN_s-4004B.champsimtrace.xz'
-    '619.lbm_s-2676B.champsimtrace.xz'
-    '619.lbm_s-2677B.champsimtrace.xz'
-    '619.lbm_s-3766B.champsimtrace.xz'
-    '619.lbm_s-4268B.champsimtrace.xz'
-    '620.omnetpp_s-141B.champsimtrace.xz'
-    '620.omnetpp_s-874B.champsimtrace.xz'
-    '621.wrf_s-6673B.champsimtrace.xz'
-    '621.wrf_s-8065B.champsimtrace.xz'
-    '623.xalancbmk_s-10B.champsimtrace.xz'
-    '623.xalancbmk_s-165B.champsimtrace.xz'
-    '623.xalancbmk_s-202B.champsimtrace.xz'
-    '623.xalancbmk_s-325B.champsimtrace.xz'
-    '623.xalancbmk_s-592B.champsimtrace.xz'
-    '623.xalancbmk_s-700B.champsimtrace.xz'
-    '627.cam4_s-490B.champsimtrace.xz'
-    '628.pop2_s-17B.champsimtrace.xz'
-    '649.fotonik3d_s-1176B.champsimtrace.xz'
-    '649.fotonik3d_s-7084B.champsimtrace.xz'
-    '649.fotonik3d_s-8225B.champsimtrace.xz'
-    '649.fotonik3d_s-10881B.champsimtrace.xz'
-    '654.roms_s-293B.champsimtrace.xz'
-    '654.roms_s-294B.champsimtrace.xz'
-    '654.roms_s-523B.champsimtrace.xz'
-    '654.roms_s-1070B.champsimtrace.xz'
-    '654.roms_s-1390B.champsimtrace.xz'
+    400.perlbench-41B.champsimtrace.xz
+    400.perlbench-50B.champsimtrace.xz
+    401.bzip2-7B.champsimtrace.xz
+    403.gcc-16B.champsimtrace.xz
+    403.gcc-17B.champsimtrace.xz
+    403.gcc-48B.champsimtrace.xz
+    433.milc-127B.champsimtrace.xz
+    444.namd-44B.champsimtrace.xz
+    445.gobmk-17B.champsimtrace.xz
+    445.gobmk-2B.champsimtrace.xz
+    464.h264ref-30B.champsimtrace.xz
+    464.h264ref-57B.champsimtrace.xz
+    464.h264ref-64B.champsimtrace.xz
+    444.namd-23B.champsimtrace.xz
+    445.gobmk-30B.champsimtrace.xz
+    445.gobmk-36B.champsimtrace.xz
+    447.dealII-3B.champsimtrace.xz
+    458.sjeng-31B.champsimtrace.xz
+    625.x264_s-12B.champsimtrace.xz
+    625.x264_s-18B.champsimtrace.xz
+    625.x264_s-20B.champsimtrace.xz
+    625.x264_s-33B.champsimtrace.xz
+    625.x264_s-39B.champsimtrace.xz
+    657.xz_s-56B.champsimtrace.xz
 )
-NUM_TRACES=${#TRACES[@]}
 
-# Terminal colors
-GREEN=$'\e[0;32m'
-RED=$'\e[0;31m'
-NC=$'\e[0m'
+mkdir -p speccpu-traces
+cd speccpu-traces || exit 1
 
-mkdir -p $DIR > /dev/null 2>&1
-echo "Download SPEC CPU2017 traces [0/$NUM_TRACES] "
-num=0
-for i in "${TRACES[@]}"; do
-    # Download spec traces
-    if [[ "$VERBOSE" == "Y" ]]; then
-        curl --progress-bar $URL$i --output $DIR/$i
-    elif [[ "$LOGGED" == "Y" ]]; then
-        curl --progress-bar $URL$i --output $DIR/$i >> $LOG 2>&1
-    else
-        curl --progress-bar $URL$i --output $DIR/$i 2>&1 | tr -d '\n'
+# Number of parallel jobs (you can also hardcode this)
+NUM_JOBS=$(nproc)
+
+# Counter to track background jobs
+job_count=0
+
+download_trace() {
+    local trace=$1
+    echo "Downloading: $trace"
+    curl -O "$BASE_URL/$trace" || echo "❌ Failed to download $trace"
+}
+
+for trace in "${TRACES[@]}"; do
+    download_trace "$trace" &
+
+    ((job_count++))
+
+    # Limit parallel jobs
+    if (( job_count >= NUM_JOBS )); then
+        wait
+        job_count=0
     fi
-    num=$(($num + 1))
-    echo "Download SPEC CPU2017 traces [$num/$NUM_TRACES]"
 done
-echo -e "Download SPEC CPU2017 traces ${GREEN}[$num/$NUM_TRACES]${NC}"
+
+# Final wait for remaining background jobs
+wait
+
+echo "✅ All downloads completed."
--- run_reuse_cache.sh
+++ run_reuse_cache.sh
@@ -5,35 +5,46 @@ export CC=~/BTP/gcc7.5/gcc-7.5.0/bin/bin/gcc
 export CXX=~/BTP/gcc7.5/gcc-7.5.0/bin/bin/g++
 
 make clean
-
 make
 
-# Path to your program executable
 PROGRAM=bin/champsim
-
-# Create an output directory if not exists
 mkdir -p reuse_cache_output
 
-# Loop through all traces in the traces/ folder
-for TRACE in traces/*.xz; do
-    # Get the base filename
-    BASENAME=$(basename "$TRACE")
+# Set how many jobs you want to run in parallel
+NUM_JOBS=$(nproc)  # or set manually, e.g., NUM_JOBS=8
+job_count=0
+
+# Function to run the simulation
+run_simulation() {
+    local TRACE="$1"
+    local BASENAME=$(basename "$TRACE")
+    local NUMBER=$(echo "$BASENAME" | sed -n 's/.*-\([0-9]\+\)B\..*/\1/p')
     
-    # Extract the number after the dash and before the 'B' (e.g., 41 from 400.perlbench-41B.champsimtrace.xz)
-    NUMBER=$(echo "$BASENAME" | sed -n 's/.*-\([0-9]\+\)B\..*/\1/p')
-
-    BILLION=1000000000
-    MILLION=1000000
-    SIMULATION=$(echo "$NUMBER * $BILLION" | bc)
-    WARMUP=$(echo "$NUMBER * $MILLION" | bc)
-
-
-    # Construct the output filename
-    OUTPUT_FILE="reuse_cache_output/${BASENAME%.xz}.out"
+    local BILLION=1000000000
+    local MILLION=1000000
+    local SIMULATION=$(echo "$NUMBER * $BILLION" | bc)
+    local WARMUP=$(echo "$NUMBER * $MILLION" | bc)
+    
+    local OUTPUT_FILE="reuse_cache_output/${BASENAME%.xz}.out"
 
     echo "Running trace $BASENAME"
     echo "$PROGRAM --warmup_instructions $WARMUP --simulation_instructions $SIMULATION -traces $TRACE > $OUTPUT_FILE"
-
-    # Run the program with the number and trace file as arguments, save output
     $PROGRAM --warmup_instructions "$WARMUP" --simulation_instructions "$SIMULATION" -traces "$TRACE" > "$OUTPUT_FILE" 2>&1
+}
+
+# Loop and run simulations in parallel
+for TRACE in traces/*.xz; do
+    run_simulation "$TRACE" &
+    ((job_count++))
+
+    # Wait for jobs if limit reached
+    if (( job_count >= NUM_JOBS )); then
+        wait
+        job_count=0
+    fi
 done
+
+# Final wait for any remaining jobs
+wait
+
+echo "✅ All simulations completed."
